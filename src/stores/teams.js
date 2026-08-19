5f05713cbb273fd28fb7470396282b7cbb6cd6db
Lukas <lukasfalisse@gmail.com>
2026-08-19 08:53:27 +0200

    add store team

---
+++ src/stores/teams.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+// fct pour utiliser le store 'teams'
+const useTeamStore = defineStore('team', {
+  // state = objet avec les données
+  state: () => {},
+  // méthodes getters
+  getters: {},
+  // méthodes setters
+  actions: {}
+})
