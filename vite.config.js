5b994d8630270947f102547b8c3c7476e63788e0
Lukas <lukasfalisse@gmail.com>
2026-08-19 08:37:37 +0200

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  //base : "/nom-dossier-ou-sera-fichier-dist/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
