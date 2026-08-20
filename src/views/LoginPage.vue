fff5facdd28f2933a584eea733bd39466bd97f99
Lukas <lukasfalisse@gmail.com>
2026-08-20 11:02:42 +0200

    add form login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -2,4 +2,18 @@
   
   <h2>Login</h2>
 
+  <div class="login-wrapper">
+
+    <form action="">
+
+      <input type="text" name="login" placeholder="login"><br>
+
+      <input type="text" name="password" placeholder="password"><br>
+
+      <button>Connection</button>
+
+    </form>
+
+  </div>
+
 </template>
