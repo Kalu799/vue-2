00d8fd78b630a833b5cc7300b3e1dacf775d0ad4
Lukas <lukasfalisse@gmail.com>
2026-08-20 11:17:14 +0200

    add connection et token

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,14 +1,40 @@
+<script setup>
+
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter()
+
+  const login = ref('')
+  const pass = ref('')
+
+  const error = ref(false)
+
+  const connexion = () => {
+    if(login.value === 'admin' && pass.value === 'pass') {
+      localStorage.token = 'montokendetest'
+      router.push('/dashboard')
+    } else {
+      error.value = true
+    }
+  }
+
+</script>
+
+
 <template>
   
   <h2>Login</h2>
 
+  <p v-show="error" class="error">Erreur de login/password</p>
+
   <div class="login-wrapper">
 
-    <form action="" class="login-form">
+    <form action="" class="login-form" @submit.prevent="connexion">
 
-      <input type="text" name="login" placeholder="login"><br>
+      <input type="text" name="login" placeholder="login" v-model.lazy.trim="login"><br>
 
-      <input type="text" name="password" placeholder="password"><br>
+      <input type="text" name="password" placeholder="password" v-model.lazy.trim="pass"><br>
 
       <button>Connection</button>
 
@@ -17,3 +43,11 @@
   </div>
 
 </template>
+
+<style scoped>
+
+  .error {
+    color: red;
+  }
+
+</style>
