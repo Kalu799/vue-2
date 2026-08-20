09d70462742f855a50ff0a38b003666a72475135
Lukas <lukasfalisse@gmail.com>
2026-08-20 08:33:02 +0200

    recup id dans TheNewsPage

--- src/views/TheNewsPage.vue
+++ src/views/TheNewsPage.vue
@@ -1,5 +1,18 @@
+<script setup>
+
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute()
+
+  console.log(route)
+
+  const id = Number(route.params.id)
+
+</script>
+
+
 <template>
 
-  <h2>One news</h2>
+  <h2>One news {{ id }}</h2>
 
 </template>
